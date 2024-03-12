2aed22161905454a28e33e0b095271a5e18a6c63
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-12 12:29:31 +0530

    add code files2

--- sample.py
+++ sample.py
@@ -10,7 +10,7 @@ def main(args=None):
     retinanet = model.resnet50(num_classes=80, pretrained=True).cuda()
     retinanet.load_state_dict(torch.load(model_path))
     example_input = torch.randn(1, 3,640,640).cuda()
-    onnx_path = "fp32.onnx"
+    onnx_path = "fp32_updated.onnx"
     torch.onnx.export(retinanet,example_input,onnx_path,opset_version=15) 
     # model being run
     # ort_session = onnxruntime.InferenceSession('/kaggle/working/retina_net_2/fp32.onnx')
