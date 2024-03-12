ea31264b0a3e5d08ab4b113c8aafefdd2d7c1a04
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-12 10:19:53 +0530

    add code files2

--- sample.py
+++ sample.py
@@ -14,24 +14,24 @@ def main(args=None):
     torch.onnx.export(retinanet,example_input,onnx_path,opset_version=15) 
     # model being run
     ort_session = onnxruntime.InferenceSession('/kaggle/working/retina_net_2/ret_new.onnx')
-    ort_inputs = {'input.1': None}
+    ort_inputs = {' input.1': None}
     dataset_val = CocoDataset('/kaggle/input/coco-2017-dataset/coco2017', set_name='val2017',
                               transform=transforms.Compose([Normalizer(), Resizer_const()])) 
     data = dataset_val[0]
     inputs = data['img'].permute(2, 0, 1).float().unsqueeze(dim=0)
-    ort_inputs['input.1'] = inputs.cpu().numpy()
+    ort_inputs[' input.1'] = inputs.cpu().numpy()
     ort_outs = ort_session.run(None, ort_inputs)
     scores, labels, boxes = ort_outs[0], ort_outs[1], ort_outs[2] 
     print(scores.shape,labels.shape,boxes.shape)
     data1 = dataset_val[1]
     inputs = data1['img'].permute(2, 0, 1).float().unsqueeze(dim=0)
-    ort_inputs['input.1'] = inputs.cpu().numpy()
+    ort_inputs[' input.1'] = inputs.cpu().numpy()
     ort_outs = ort_session.run(None, ort_inputs)
     scores, labels, boxes = ort_outs[0], ort_outs[1], ort_outs[2] 
     print(scores.shape,labels.shape,boxes.shape)
     data2 = dataset_val[2]
     inputs = data2['img'].permute(2, 0, 1).float().unsqueeze(dim=0)
-    ort_inputs['input.1'] = inputs.cpu().numpy()
+    ort_inputs[' input.1'] = inputs.cpu().numpy()
     ort_outs = ort_session.run(None, ort_inputs)
     scores, labels, boxes = ort_outs[0], ort_outs[1], ort_outs[2] 
     print(scores.shape,labels.shape,boxes.shape)   
