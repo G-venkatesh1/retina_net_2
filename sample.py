52f308c5ef2d99082426cd141c932718a7fc96e1
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-12 09:52:19 +0530

    add code files2

--- sample.py
+++ sample.py
@@ -9,7 +9,7 @@ def main(args=None):
     model_path ='/kaggle/input/retina_net/pytorch/model/1/coco_resnet_50_map_0_335_state_dict.pt'
     retinanet = model.resnet50(num_classes=80, pretrained=True).cuda()
     retinanet.load_state_dict(torch.load(model_path))
-    example_input = torch.randn(1, 3,640,640).cuda()
+    example_input = torch.randn(1, 3,1024,1024).cuda()
     onnx_path = "ret_new.onnx"
     torch.onnx.export(retinanet,example_input,onnx_path,opset_version=15)                              # model being run
     ort_session = onnxruntime.InferenceSession('/kaggle/working/retina_net_2/ret_new.onnx')
